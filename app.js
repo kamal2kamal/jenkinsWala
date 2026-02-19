892fabb3a3fe107ead3f7a78044ad6eb876167fc
kamal2kamal <karthikt1346@gmail.com>
2026-02-19 17:25:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const http = require('http');
         const server = http.createServer((req, res) => {
-          res.end('Hello! I am now listening to the whole world on 0.0.0.0');
+          res.end('Hello! I am now listening to the whole world on 0.0.0.0 with port 8084');
         });
         // We MUST use '0.0.0.0' so Windows can see it!
         server.listen(3000, '0.0.0.0', () => {
